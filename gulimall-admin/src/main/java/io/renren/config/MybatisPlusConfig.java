4f5b6eba90c4ade265f45953fb4341e8c61c476a
Zerek Weng <zerekweng@gmail.com>
2024-02-19 00:41:55 -0800

    add admin manage system

---
+++ gulimall-admin/src/main/java/io/renren/config/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 指定数据库方言为 MYSQL
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ gulimall-admin/src/main/java/io/renren/modules/app/form/RegisterForm.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.modules.app.form;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * 注册表单
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Data
+@Schema(title = "注册表单")
+public class RegisterForm {
+    @Schema(name = "手机号")
+    @NotBlank(message="手机号不能为空")
+    private String mobile;
+
+    @Schema(name = "密码")
+    @NotBlank(message="密码不能为空")
+    private String password;
+
+}
